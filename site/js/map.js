e7753e76faa263f3c5abe81c12f760ec791f8a21
Max Kelly <maxwill.kelly@gmail.com>
2020-02-26 15:19:59 +0000

    Base Site Code

---
+++ site/js/map.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWF4d2lsbGtlbGx5IiwiYSI6ImNrNjhsOWdlZTA0M2Yza21mMG9icjBwdmIifQ.OTaUkNePX-6XE3Vgcy9v6A";
+
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/light-v10",
+  zoom: 4.5,
+  center: [-4.2, 57.2]
+});
