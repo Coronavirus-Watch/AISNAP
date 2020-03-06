1c74773ab792a3f95e0c69d27b45e1345d3f3162
Max Kelly <maxwill.kelly@gmail.com>
2020-03-06 20:02:37 +0000

    Begins work on nodejs timeline code

---
+++ timeline/timeline.js
@@ -0,0 +1,13 @@
+// Data source: https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports
+
+const http = require('http');
+const fs = require('fs'); 
+const readline = require('readline');
+const url = require('url');
+
+var path = "../data/timeline";
+
+http.createServer(function (req, res) {
+    // Make directory for source files
+    //
+}).listen(8080); 
