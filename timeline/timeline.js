bfc1d65677340d18485fa3d4daac5c7173b8c14a
Max Kelly <maxwill.kelly@gmail.com>
2020-03-12 14:36:02 +0000

    Wierd merge bug

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -143,9 +143,6 @@ function download() {
         date.setDate(date.getDate() + 1);
     }
 }
-const tempPath = '../data/tmp';
-const source =
-	'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/';
 
 // 
 function objectification() {
