c482394463e09e74f6132624e482949cd539a05d
Ramsay Sewell <sewellrf@gmail.com>
2020-03-13 22:04:48 +0000

    updated visuals and timeline (sorry max)

--- js/Routes.js
+++ js/Routes.js
@@ -1,23 +1,15 @@
-import { Country } from './Country.js';
+import { Countries } from './Countries.js';
 export class Routes {
 	constructor() {
-		this.countries = this.fetchCountries(); // fetch countries.txt
-		this.routes = this.fetchRoutes();
-		console.log('hello');
+		this.allCountries = new Countries();
+		this.routes = [];
+		this.geojson = [];
 	}
 
-	async fetchCountries() {
-		// retrieves data from countries file
-		const response = await fetch('../data/countries.txt');
-		// parses data into text
-		const data = await response.text();
-		//
-		this.parseCountries(data);
-	}
-
-	async parseCountries(text) {
-		// splits the text into lines in the file
-		const lines = text.split('\n');
+	async init() {
+		await this.allCountries.init();
+		await this.fetchRoutes();
+		await this.allCountries.parseGeoJSON();
 	}
 
 	// fetches routes from text file
@@ -29,7 +21,6 @@ export class Routes {
 		// parses the data into a routes Object
 		const routes = await this.parseRoutes(text);
 		// returns Object of all routes
-		console.log(routes);
 		this.routes = routes;
 	}
 
@@ -41,62 +32,76 @@ export class Routes {
 		// goes through each route in the file
 		for (let i = 0; i < lines.length; i++) {
 			// splits each route into a "journey"
-			this.journey = lines[i].split(',');
-
-			// checks if the the journey start and end location both exist
+			const journey = lines[i].split(',');
 			if (
-				sources.countries[journey[1]] != undefined &&
-				sources.countries[journey[3]] != undefined
+				this.allCountries.countries[journey[3]] !== undefined &&
+				this.allCountries.countries[journey[1]] !== undefined
 			) {
-				// creates coordinate arrays for both start and end locations
-				this.fromCoord = {
-					lat: parseFloat(sources.countries[journey[1]].lon),
-					lon: parseFloat(sources.countries[journey[1]].lat)
-				};
-				this.toCoord = {
-					lat: parseFloat(sources.countries[journey[3]].lon),
-					lon: parseFloat(sources.countries[journey[3]].lat)
-				};
+				const fromCoord = [
+					parseFloat(
+						this.allCountries.countries[journey[1]].coordinates[0]
+					),
+					parseFloat(
+						this.allCountries.countries[journey[1]].coordinates[1]
+					)
+				];
+				const toCoord = [
+					parseFloat(
+						this.allCountries.countries[journey[3]].coordinates[0]
+					),
+					parseFloat(
+						this.allCountries.countries[journey[3]].coordinates[1]
+					)
+				];
+
 				const origin = {
-					name: '',
-					lat: this.fromCoord.lat,
-					lon: this.toCoord.lon
+					name: journey[1].trim(),
+					coordinates: fromCoord
 				};
 				const destination = {
-					name: '',
-					lat: this.toCoord.lat,
-					lon: this.toCoord.lon
+					name: journey[3].trim(),
+					coordinates: toCoord
 				};
-				this.routes.push(new Route(origin, destination));
+				const newRoute = new Route(origin, destination);
+				this.routes.push(newRoute);
 			}
+		}
+		return this.routes;
+	}
 
-			// pushes a new (MapBox) "Feature" to the features array
-			sources['routes'].push({
+	parseGeoJSON() {
+		let tempArray = [];
+		this.routes.forEach(route => {
+			tempArray.push({
 				type: 'Feature',
 				geometry: {
 					type: 'LineString',
-					coordinates: [fromCoord, toCoord]
+					coordinates: [
+						route.origin.coordinates,
+						route.destination.coordinates
+					]
 				}
 			});
-		}
+		});
+		this.geojson = tempArray;
 	}
 }
 
 class Route {
-	constructor(origin, destination) {
+	constructor(newOrigin, newDestination) {
 		this.origin = {
-			name: origin.name,
-			coordinates: {
-				lat: origin.lat,
-				lon: origin.lon
-			}
+			name: newOrigin.name,
+			coordinates: newOrigin.coordinates
 		};
 		this.destination = {
-			name: destination.name,
-			coordinates: {
-				lat: destination.lat,
-				lon: destination.lon
-			}
+			name: newDestination.name,
+			coordinates: newDestination.coordinates
 		};
+	}
+	getOrigin() {
+		return this.origin;
+	}
+	getDestination() {
+		return this.destination;
 	}
 }
--- js/map.js
+++ js/map.js
@@ -1,3 +1,5 @@
+import { Routes } from './Routes.js';
+
 /* Useful resources:
   https://dev.to/wuz/building-a-country-highlighting-tool-with-mapbox-2kbh
   https://bl.ocks.org/danswick/fc56f37c10d40be62e4feac5984250d2
@@ -22,17 +24,10 @@ let toggledLayers = {
 	checkedRadio: 'cases'
 };
 
-// Default (MapBox) Markers and Features data structures
-let sources = {
-	countries: {},
-	routes: [],
-	geoCountries: []
-};
-
 // GUI elements
 const routesCheck = document.querySelector('#routes');
 const markersCheck = document.querySelector('#markers');
-const checked = document.querySelector('input[type=radio]:checked');
+const radioBtns = document.querySelectorAll('input[type=radio]');
 const dateSlider = document.getElementById('dateSlider');
 const dateDisplayed = document.getElementById('dateDisplayed');
 dateSlider.max = 100;
@@ -48,7 +43,6 @@ markersCheck.addEventListener('change', e => {
 	updateMap(toggledLayers);
 });
 
-const radioBtns = document.querySelectorAll('input[type=radio]');
 radioBtns.forEach(btn => {
 	btn.addEventListener('change', e => {
 		toggledLayers.checkedRadio = e.target.value;
@@ -56,11 +50,13 @@ radioBtns.forEach(btn => {
 	});
 });
 // when map first loads on webpage
+let routes = new Routes();
 map.on('load', async () => {
-	const Routes = new Routes();
 	// collects all markers in an array
-	const markers = await fetchMarkers();
+	// const markers = await fetchMarkers();
 
+	await routes.init();
+	await routes.parseGeoJSON();
 	// collects all routes in an Object
 	// const routes = await fetchRoutes(markers);
 	// Adds routes to layers
@@ -72,116 +68,24 @@ map.on('load', async () => {
 	updateMap(toggledLayers);
 });
 
-// Fetches Location Data of Countries
-const fetchMarkers = async () => {
-	// retrieves data from countries file
-	const response = await fetch('../data/countries.txt');
-	// parses data into text
-	const data = await response.text();
-	// parses the data into list of markers
-	const markers = parseMarkers(data);
-	// returns an array of (MapBox) Markers
-	return markers;
-};
-
-// Parses the marker text data
-function parseMarkers(text) {
-	// splits the text into lines in the file
-	const lines = text.split('\n');
-	// Loops through each line
-	for (let i = 0; i < lines.length; i++) {
-		// Extracts each line
-		const countryLine = lines[i].split(',');
-		// Adds to the markers Object with respective lat, lon based on the country
-		sources.countries[countryLine[3].trim()] = {
-			lat: countryLine[1].trim(),
-			lon: countryLine[2].trim()
-		};
-		let lon = parseFloat(countryLine[2].trim());
-		let lat = parseFloat(countryLine[1].trim());
-		sources['geoCountries'].push({
-			type: 'Feature',
-			geometry: {
-				type: 'Point',
-				coordinates: [lon, lat]
-			},
-			properties: {
-				title: countryLine[3].trim(),
-				icon: 'playground'
-			}
-		});
-	}
-}
-
-import { Routes } from './Routes.js';
-const routes = new Routes();
-
-// fetches routes from text file
-// const fetchRoutes = async data => {
-// 	// retrieves text data from file
-// 	const res = await fetch('../data/parsedDomesticsOutput.txt');
-// 	// parses the response data into plain text
-// 	const text = await res.text();
-// 	// parses the data into a routes Object
-// 	const routes = await parseRoutes(text);
-
-// 	// returns Object of all routes
-// 	return routes;
-// };
-
-// // Parses the routes text data
-// const parseRoutes = text => {
-// 	// splits text up into lines in the file
-// 	const lines = text.split('\n');
-
-// 	// goes through each route in the file
-// 	for (let i = 0; i < lines.length; i++) {
-// 		// splits each route into a "journey"
-// 		let journey = lines[i].split(',');
-
-// 		// checks if the the journey start and end location both exist
-// 		if (
-// 			sources.countries[journey[1]] != undefined &&
-// 			sources.countries[journey[3]] != undefined
-// 		) {
-// 			// creates coordinate arrays for both start and end locations
-// 			let fromCoord = [
-// 				parseFloat(sources.countries[journey[1]].lon),
-// 				parseFloat(sources.countries[journey[1]].lat)
-// 			];
-// 			let toCoord = [
-// 				parseFloat(sources.countries[journey[3]].lon),
-// 				parseFloat(sources.countries[journey[3]].lat)
-// 			];
-
-// 			// pushes a new (MapBox) "Feature" to the features array
-// 			sources['routes'].push({
-// 				type: 'Feature',
-// 				geometry: {
-// 					type: 'LineString',
-// 					coordinates: [fromCoord, toCoord]
-// 				}
-// 			});
-// 		}
-// 	}
-// };
-
 // Adds Layers to Map
 function addLayers() {
 	// Adds new source for routes
+	if (routes.geojson.length == 0) {
+		console.log('sugar me timbers');
+	}
 	map.addSource('route', {
 		type: 'geojson',
 		data: {
 			type: 'FeatureCollection',
-			features: sources.routes
+			features: routes.geojson
 		}
 	});
-
 	map.addSource('country', {
 		type: 'geojson',
 		data: {
 			type: 'FeatureCollection',
-			features: sources.geoCountries
+			features: routes.allCountries.geojson
 		}
 	});
 
